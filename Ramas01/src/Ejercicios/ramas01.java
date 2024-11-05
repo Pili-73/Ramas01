246453a6ca37fa6254ca4dc8dfb12ca93fb49972
pilar <piruletaabad2002@gmail.com>
2024-11-05 12:44:30 +0100

    añadidas funciones

--- Ramas01/src/Ejercicios/ramas01.java
+++ Ramas01/src/Ejercicios/ramas01.java
@@ -5,38 +5,56 @@ import java.util.Scanner;
 public class ramas01 {
 
 	public static void main(String[] args) {
-		
-		int contNotas = 0;
-		double maxNota = 11, minNota = 0;
-		
-		Scanner sc = new Scanner(System.in);
-		int opcion = Funciones.dimeEntero(
-				"1. Agregar nota de un alumno.\n 2. Calcular promedio. \n3. Mostrar nota más alta. \n4. Mostrar nota más baja. \n0. Salir.", sc);
-		/*
-		if (opcion == 1) {
-			agregarNota(double nota, sc);
-		} else if (opcion == 2) {
-			calcularPromedio(double nota);
-		} else if (opcion == 3) {
-			notaMaxima();
-		}else if (opcion == 1) {
-			notaMinima();
-		}*/
+
+		int totalAlumnos = 0, opcion = -1;
+		double maxNota = -1, minNota = 11, sumNota = 0;
+
+		do {
+			Scanner sc = new Scanner(System.in);
+			opcion = Funciones.dimeEntero(
+					"1. Agregar nota de un alumno.\n2. Calcular promedio. \n3. Mostrar nota más alta. \n4. Mostrar nota más baja. \n0. Salir.",
+					sc);
+
+			if (opcion == 1) {
+				agregarNota(totalAlumnos, sumNota, maxNota, minNota, sc);
+
+			} else if (opcion == 2) {
+				calcularPromedio(totalAlumnos, sumNota);
+
+			} else if (opcion == 3) {
+				notaMaxima(maxNota);
+
+			} else if (opcion == 4) {
+				notaMinima(minNota);
+
+			} else if (opcion != 0) {
+				System.out.println("Entre 0 y 4");
+			}
+		} while (opcion != 0);
 	}
 
-}
+	public static void agregarNota(int totalAlumnos, double sumNota, double maxNota, double minNota, Scanner sc) {
+		double nota;
+		do {
+			nota = Funciones.dimeDouble("Escribe la nota", sc);
+			if (nota >= 0 && nota <= 10) {
+				totalAlumnos++;
+				sumNota += nota;
+				maxNota = Math.max(maxNota, nota);
+				minNota = Math.min(minNota, nota);
+			}
+		} while (nota > 10 || nota < 0);
+	}
 
-/*
-public static void agregarNota(double nota, sc) {
-	double nota = Funciones.dimeDouble("Escribe la nota", sc);
-	
+	public static void calcularPromedio(int totalAlumnos, double sumNota) {
+		System.out.println(sumNota / totalAlumnos);
+	}
+
+	public static void notaMaxima(double maxNota) {
+		System.out.println(maxNota);
+	}
+
+	public static void notaMinima(double minNota) {
+		System.out.println(minNota);
+	}
 }
-public static void calcularPromedio(int totalAlumnos) {
-	
-}
-public static void notaMaxima() {
-	
-}
-public static void notaMinima() {
-	
-}*/
